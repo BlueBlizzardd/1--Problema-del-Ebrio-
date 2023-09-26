1f8070b2ac9a726a034f8825ed2adc5a58752a5f
BlueBlizzardd <105523256+BlueBlizzardd@users.noreply.github.com>
2023-09-26 16:51:58 -0400

    v1.0

---
+++ assets/js/solution.js
@@ -0,0 +1,25 @@
+const html = document.querySelector(".result"); 
+const container = document.querySelector(".grid");
+let sims = prompt("Selecciona numero de simulaciones: ");
+let res = 1;
+
+if(sims > 0) {
+    for(let i = 0; i < sims; i++) {
+        res = res * (16 / 441);
+    }
+
+    html.innerHTML = `La probabilidad resultante es de ${res.toString()} o ${(res*100).toFixed(10).toString()}%`;
+} else {
+    alert("El numero de simulaciones tiene que ser mayor que 0");
+}
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(21, 21);
